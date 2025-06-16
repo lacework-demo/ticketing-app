d007e51ef5903ff3ed85f901bf5e1404f305d730
Gabe O'Brien <gabe.obrien@lacework.net>
2025-06-16 13:20:20 -0700

    fix: bump ubuntu os version

--- deploy/terraform/gcp/modules/gcpcompute/main.tf
+++ deploy/terraform/gcp/modules/gcpcompute/main.tf
@@ -33,7 +33,7 @@ resource "google_compute_instance" "instance-server" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
       labels = var.disk_labels
     }
   }
